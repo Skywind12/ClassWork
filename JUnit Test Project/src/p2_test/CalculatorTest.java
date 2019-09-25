0f67797e5ba1dd6c231d5abc4227dec663448dce
Jahleel Martinez Fleming <pachimiro99@gmail.com>
2019-09-25 14:03:49 -0400

    Add files via upload
    9/25/2019 Class Work JUnit

---
+++ JUnit Test Project/src/p2_test/CalculatorTest.java
@@ -0,0 +1,37 @@
+package p2_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import p2.Calculator;
+
+
+class CalculatorTest {
+
+	@Test
+	void testTime() {
+		Calculator c = new Calculator(5);
+		c.time(2);
+		assertEquals(10,c.getAmount(),0.01);
+	}
+	
+	@Test
+	void testDivide() {
+		Calculator c = new Calculator(10);
+		c.divide(0);
+		assertEquals(5,c.getAmount(),0.0000001);
+	}
+	
+	@Test
+	void roundAmount() {
+		Calculator c = new Calculator(1);
+		System.out.println(c.roundAmount(1234.12345));
+		assertEquals("1,234.12", c.roundAmount(1234.12345));
+	}
+	@Test
+	void testDenominatorIsZero() {
+		Calculator c = new Calculator(5);
+		assertTrue(c.denominatorIsZero(0));
+	}
+}
